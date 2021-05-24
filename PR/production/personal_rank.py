38aa33b9d232516b8351b052ebb2f2fa6ce46a1b
fivelike <1875265593@qq.com>
2021-05-24 19:15:40 +0800

    pr_mat

--- PR/production/personal_rank.py
+++ PR/production/personal_rank.py
@@ -1,4 +1,4 @@
-#-*-coding:utf8-*-
+# -*-coding:utf8-*-
 """
 author: fivelike
 date: 2021.5.17
@@ -7,9 +7,15 @@ personal rank main algo
 from tqdm import tqdm
 import operator
 import sys
+
 sys.path.append("../util")
 import util.read as read
-def personal_rank(graph, root, alpha, iter_num, recom_num = 10):
+import util.mat_util as mat_util
+from scipy.sparse.linalg import gmres
+import numpy as np
+
+
+def personal_rank(graph, root, alpha, iter_num, recom_num=10):
     """
     非矩阵化实现 personal rank 算法
     :param graph: user item graph
@@ -27,24 +33,62 @@ def personal_rank(graph, root, alpha, iter_num, recom_num = 10):
         tmp_rank = {point: 0 for point in graph}
         for out_point, out_dict in graph.items():
             for inner_point, value in graph[out_point].items():
-                tmp_rank[inner_point] += round(alpha*rank[out_point]/len(out_dict),4)
+                tmp_rank[inner_point] += round(alpha * rank[out_point] / len(out_dict), 4)
                 if inner_point == root:
-                    tmp_rank[inner_point] += round(1-alpha,4)
+                    tmp_rank[inner_point] += round(1 - alpha, 4)
         if tmp_rank == rank:
+            print('out' + str(iter_index))
             break
         rank = tmp_rank
+
     right_num = 0
-    for co in sorted(rank.items(), key = operator.itemgetter(1),reverse=True):
+    for co in sorted(rank.items(), key=operator.itemgetter(1), reverse=True):
         point, pr_score = co[0], co[1]
-        if len(point.split('_'))<2:
+        if len(point.split('_')) < 2:
             continue
         if point in graph[root]:
             continue
         recom_result[point] = pr_score
-        right_num +=1
-        if right_num > recom_num:
+        right_num += 1
+        if right_num >= recom_num:
             break
     return recom_result
+
+
+def personal_rank_matrix(graph, root, alpha, recom_num=10):
+    """
+    :param graph: user item graph
+    :param root: the fix user to recom
+    :param alpha: the prob to random walk
+    :param recom_num: recom item num
+    :return: a dict, key itemid, value pr score
+    """
+    m, vertex, address_dict = mat_util.graph_to_m(graph)
+    if root not in address_dict:
+        return {}
+    score_dict = {}
+    recom_dict = {}
+
+    mat_all = mat_util.mat_all_point(m, vertex, alpha)
+    index = address_dict[root]
+    initial_list = [[0] for i in range(len(vertex))]
+    initial_list[index] = [1]
+    r_zero = np.array(initial_list)
+
+    res = gmres(mat_all, r_zero, tol=1e-8)[0]
+
+    for i in range(len(res)):
+        point = vertex[i]
+        if len(point.strip().split('_')) < 2:
+            continue
+        if point in graph[root]:
+            continue
+        score_dict[point] = round(res[i], 3)
+    for co in sorted(score_dict.items(), key=operator.itemgetter(1), reverse=True)[:recom_num]:
+        point, score = co[0], co[1]
+        recom_dict[point] = score
+    return recom_dict
+
 
 def get_one_user_recom():
     """
@@ -54,7 +98,8 @@ def get_one_user_recom():
     alpha = 0.8
     graph = read.get_graph_from_data("../data/ratings.csv")
     iter_num = 100
-    recom_result = personal_rank(graph,user,alpha,iter_num)
+    recom_result = personal_rank(graph, user, alpha, iter_num, 100)
+    return recom_result
     """
     # 将推荐结果直接存盘或写入到KV中
     item_info = read.get_item_info("../data/movies.csv")
@@ -68,6 +113,24 @@ def get_one_user_recom():
         print(recom_result[itemid])
     """
 
-if __name__ == '__main__':
-    get_one_user_recom()
 
+def get_one_user_by_mat():
+    """
+    give one fix user by mat
+    """
+    user = "1"
+    alpha = 0.8
+    graph = read.get_graph_from_data("../data/ratings.csv")
+    recom_result = personal_rank_matrix(graph, user, alpha, 100)
+    return recom_result
+
+
+if __name__ == '__main__':
+    recom_result_base = get_one_user_recom()
+    recom_result_mat = get_one_user_by_mat()
+    num = 0
+    for i in recom_result_base:
+        if i in recom_result_mat:
+            # print(i)
+            num += 1
+    print("迭代版本与矩阵版本top100相同数：", num)
--- PR/util/read.py
+++ PR/util/read.py
@@ -1,10 +1,12 @@
-#-*-coding:utf8-*-
+# -*-coding:utf8-*-
 """
 author: fivelike
 date: 2021.5.17
 get graph from user data
 """
 import os
+
+
 def get_graph_from_data(input_file):
     """
     :param input_file: user item rating file
@@ -14,17 +16,17 @@ def get_graph_from_data(input_file):
     if not os.path.exists(input_file):
         return {}
     graph = {}
-    linenum=0
+    linenum = 0
     score_thr = 4.0
     fp = open(input_file, encoding="utf-8")
     for line in fp:
-        if linenum==0:
-            linenum+=1
+        if linenum == 0:
+            linenum += 1
             continue
         item = line.strip().split(",")
-        if len(item)<3:
+        if len(item) < 3:
             continue
-        userid, itemid, rating = item[0], "item_"+item[1], float(item[2])
+        userid, itemid, rating = item[0], "item_" + item[1], float(item[2])
         if rating < score_thr:
             continue
         if userid not in graph:
@@ -35,6 +37,7 @@ def get_graph_from_data(input_file):
         graph[itemid][userid] = 1
     fp.close()
     return graph
+
 
 def get_item_info(input_file):
     """
@@ -62,12 +65,12 @@ def get_item_info(input_file):
             itemid = item[0]
             genre = item[-1]
             title = ",".join(item[1: -1])
-        item_info[itemid] = [title,genre]
+        item_info[itemid] = [title, genre]
     fp.close()
     return item_info
+
 
 if __name__ == '__main__':
     # print(get_graph_from_data("../data/test.txt"))
     graph = get_graph_from_data("../data/ratings.csv")
     print(graph["1"])
-
